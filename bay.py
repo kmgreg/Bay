82deb45c26ba2a27a408501e10d2ed604f384a8d
Kurt Gregorek <kxg1491@g.rit.edu>
2017-12-08 15:27:57 -0500

    does the cdf I think

--- bay.py
+++ bay.py
@@ -3,9 +3,13 @@ import math as math
 import numpy as np
 
 # magic number list
-px = 13.6821049679
+px = -10875.331581
 r = 6.81504915679406
 
+ancient = 0
+ancientl = 0
+
+#the original function, using x, lambda(l) and mu (m)
 def evaluate(x,l,m):
     term1 = (l*(1/math.sqrt(2*math.pi))*(math.pow(math.e,(-(math.pow(x,2))/2))))
     term2 = ((1 - l)*(1/(math.sqrt(2*math.pi*(.1*.1))))*(math.pow(math.e,(-((x-m)*(x-m))/(2*(.1*.1))))))
@@ -23,31 +27,32 @@ def loglikelihood(l,m):
     return goodlog(old)
     #return np.power(np.e,goodlog(old))
 
+
+#the offset to reduce underflow
 def goodlog(value):
     return value + 5000
 
+#a left reimann sum of the loglikehood
 def rsum(m):
+    list = []
     sum = 0
-    val = 0.005
+    val = 0.01
     for x in range(0,int(1/val)):
         sum = sum + (loglikelihood(x*val,m)* val)
-        #print(sum)
-    return sum
-
-def lcdf(l,m):
-    return np.power(np.e,loglikelihood(l,m) + np.log(r) - np.log(px))
+        list.append(sum/px)
+    return list
 
 ne = np.vectorize(evaluate)
 nl = np.vectorize(loglikelihood)
-vl = np.vectorize(lcdf)
-c = np.arange(-5,5, .05)
+r = np.vectorize(rsum)
+m = np.arange(0,1,.01)
+#range(-5,5, .05)
 l = np.arange(0.05,1,.05)
-m = np.arange(0,1,.05)
-#plt.title('ln L vs lambda', fontsize=20)
-#plt.xlabel('Value of Lambda')
-#plt.ylabel('Log Likelihood')
+plt.title('CDF of Lambda', fontsize=20)
+plt.xlabel('Value of Lambda')
+plt.ylabel('CDF')
 #plt.plot(c,ne(c,.9,3))
 #plt.plot(l,nl(l,3))
-#plt.plot(m,vl(m,3))
-#plt.show()
-print(rsum(3))
+plt.plot(m,r(3))
+plt.show()
+#print(rsum(3))
